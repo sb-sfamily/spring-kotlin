6a442917f5612b9e82fd907ea03dece3ac9afa00
Park SeungBin <sb06park@gmail.com>
2024-05-12 10:25:14 +0900

    remove option

--- src/main/kotlin/family/park/devops/cloud/messenger/web/IndexController.kt
+++ src/main/kotlin/family/park/devops/cloud/messenger/web/IndexController.kt
@@ -1,8 +1,6 @@
 package family.park.devops.cloud.messenger.web
 
 import org.springframework.web.bind.annotation.*
-import java.util.LinkedHashMap
-import javax.print.DocFlavor.STRING
 
 @RestController
 @RequestMapping("/index")
@@ -12,8 +10,21 @@ class IndexController {
     @GetMapping
     fun index() = mapOf("greeting word" to "Hello World")
 
+    @GetMapping("state")
+    fun listState(): List<String> {
+        return studentList
+    }
+
     @PostMapping("/create")
     fun addStudent(@RequestBody map: Map<String, String>) = studentList.apply {
         add(map["name"]!!)
+    }
+
+    @PostMapping("/delete")
+    fun deleteStudent(@RequestBody map: Map<String, String>) = studentList.apply {
+        if(studentList.isEmpty()){
+            println("no student list")
+        }
+        removeLast()
     }
 }
