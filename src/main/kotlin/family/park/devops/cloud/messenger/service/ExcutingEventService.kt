35a1d20504ac259f8ba97df4761a47165caffb1d
Park SeungBin <sb06park@gmail.com>
2024-06-15 10:45:43 +0900

    tried to add inserting service, but error and I can not fix this

--- src/main/kotlin/family/park/devops/cloud/messenger/service/ExcutingEventService.kt
+++ src/main/kotlin/family/park/devops/cloud/messenger/service/ExcutingEventService.kt
@@ -3,12 +3,12 @@ package family.park.devops.cloud.messenger.service
 import family.park.devops.cloud.messenger.entity.InsertExecutingEvent
 import family.park.devops.cloud.messenger.repo.ExcutingEventRepo
 
-class ExcutingEventService(
-    private val excutingEventRepo : ExcutingEventRepo,
+class ExcuteEventService(
+    private val excuteEventRepo : ExcutingEventRepo,
     repo : ExcutingEventRepo
 ) {
 
     fun insertExcutingEvent(event : InsertExecutingEvent)
-    =excutingEventRepo.insertsExtraEvents(event)
+    =excuteEventRepo.insertsExtraEvents(event)
 
 }
--- src/main/kotlin/family/park/devops/cloud/messenger/web/ExcutingEventController.kt
+++ src/main/kotlin/family/park/devops/cloud/messenger/web/ExcutingEventController.kt
@@ -1,18 +1,17 @@
-package family.park.devops.cloud.messenger.web
-
-import family.park.devops.cloud.messenger.entity.InsertExecutingEvent
-import family.park.devops.cloud.messenger.model.InsertEvent
-import family.park.devops.cloud.messenger.service.ExcutingEventService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
-
-@RestController
-@RequestMapping("/v1/excutingEvent")
-class ExcutingEventController (private val excutingEventService : ExcutingEventService) {
-
-    @PostMapping("/insert")
-    fun insertListEvents(@RequestBody insertEvents : InsertExecutingEvent) = excutingEventService.insertListEvent(insertEvents)
-}
+//package family.park.devops.cloud.messenger.web
+//
+//import family.park.devops.cloud.messenger.entity.InsertExecutingEvent
+//import family.park.devops.cloud.messenger.service.ExcuteEventService
+//import org.springframework.web.bind.annotation.PostMapping
+//import org.springframework.web.bind.annotation.RequestBody
+//import org.springframework.web.bind.annotation.RequestMapping
+//import org.springframework.web.bind.annotation.RestController
+//
+//
+//@RestController
+//@RequestMapping("/v1/excutingEvent")
+//class ExcutingEventController (private val excuteEventService: ExcuteEventService) {
+//
+//    @PostMapping("/insert")
+//    fun insertListEvents(@RequestBody insertEvents : InsertExecutingEvent) = excuteEventService.insertExcutingEvent(insertEvents)
+//}
